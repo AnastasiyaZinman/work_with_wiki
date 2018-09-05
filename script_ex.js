5a671fda77bfe6d304766c8f9ce439996a8b79aa
AGCH <anastasiyache@gmail.com>
2018-09-05 10:03:02 +0300

    with render

--- script_ex.js
+++ script_ex.js
@@ -1,7 +1,7 @@
 const SEARCHING_POPULATION_TEXT = "Population•&#160;",
   SEARCHING_AREA_TEXT = "Area •&#160;",
   SEARCHING_FLAG_TEXT = 'src=\\"',
-  SEARCHING_CAPITAL_TEXT='Capital\n';
+  SEARCHING_CAPITAL_TEXT = 'Capital\n';
 $('button').on('click', function () {
   $('.book-list').empty();
   var title = $('input').val();
@@ -9,10 +9,10 @@ $('button').on('click', function () {
   fetch(title);
 })
 
-function findCapital(str,searching_text) {
+function findCapital(str, searching_text) {
   var searching_answer = '', current_pos;
   // console.log("find", str.replace(/Capitaland(.*?)\n/, 'Capital\n'));
-  str=str.replace(/Capitaland(.*?)\n/, searching_text);
+  str = str.replace(/Capitaland(.*?)\n/, searching_text);
   //-------------------------
   // var dict = {"á":"a", "á":"a", "ç":"c","é":"e","í":"i"};
   // str.replace(/[^\w ]/g, function(char) {
@@ -27,7 +27,7 @@ function findCapital(str,searching_text) {
       current_pos++;
     }
     return searching_answer;
-  } 
+  }
   else return null;
 }
 
@@ -42,16 +42,33 @@ function findCountryInfo(str, searching_text, signal_sign) {
     }
     return searching_answer;
   }
- else return null;
+
+  return null;
+}
+var AppendData = function (data) {
+  var source = $('#country-template').html();
+  var template = Handlebars.compile(source);
+  //---Second Checking entered title
+  console.log(data);
+  if (data.length === 0)
+    $('.book-list').append("Can't find book with entered title.");
+  else {
+    console.log("handlebars");
+    var newHTML = template(data);
+    $('.book-list').append(newHTML);
+
+  }
 }
 
-function findInfo(data) {
+function findInfo(data, title) {
   var answer = {};
-  answer["capital"] = findCapital(data.text,SEARCHING_CAPITAL_TEXT);
+  answer["country"] = title;
+  answer["capital"] = findCapital(data.text, SEARCHING_CAPITAL_TEXT);
   answer["population"] = findCountryInfo(data.text, SEARCHING_POPULATION_TEXT, '&');
   answer["area"] = findCountryInfo(data.text, SEARCHING_AREA_TEXT, '&');
-  answer["flag_path"] = findCountryInfo(JSON.stringify(data), SEARCHING_FLAG_TEXT, '\\');
+  answer["flag_path"] = 'https:'+findCountryInfo(JSON.stringify(data), SEARCHING_FLAG_TEXT, '\\');
   console.log(answer);
+  AppendData(answer);
 }
 
 var fetch = function (title) {
@@ -63,7 +80,7 @@ var fetch = function (title) {
       if (JSON.stringify(data) !== 'null') {
         // console.log((data[0].text));
         // console.log(data[0].html);
-        findInfo(data[0]);
+        findInfo(data[0], title);
       }
       else alert('Enter another country');
     },
